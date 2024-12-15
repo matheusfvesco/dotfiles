fd8e027e8738c4aea8c8894331e94e376139b4ab
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2024-12-15 01:43:33 -0300

    Add refind script

---
+++ bin/install_refind.sh
@@ -0,0 +1,6 @@
+sudo apt-add-repository ppa:rodsmith/refind
+sudo apt-get update
+sudo apt-get install refind
+
+sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/bobafetthotmail/refind-theme-regular/master/install.sh)"
+
