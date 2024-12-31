ff9fa4ad015c4b378d7d93ce4249a197296db254
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2024-12-31 14:38:03 -0300

    Fix cuda install

--- bin/cuda_post_install.sh
+++ bin/cuda_post_install.sh
@@ -1,7 +1,7 @@
 # mandatory
-echo "export PATH=/usr/local/cuda-12.6/bin${PATH:+:${PATH}}" >> ~/.zshrc
+echo 'export PATH=/usr/local/cuda-12.6/bin${PATH:+:${PATH}}' >> ~/.zshrc
 
-echo "export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> .zshrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> .zshrc
 
 sudo apt update
 sudo apt-get install g++ freeglut3-dev build-essential libx11-dev \
